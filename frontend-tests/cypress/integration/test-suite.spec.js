8857ce05e98be066afed51581e9218ef55a2ae77
Ibirocay Regueira <ibirocay.regueira@yh.nackademin.se>
2021-10-31 23:58:27 +0100

    Created a .gitignore and the assignment setup

---
+++ frontend-tests/cypress/integration/test-suite.spec.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+//This is a test suite
+describe('test suite', function(){
+    //This is a test case (not included in assignment, only used for login purpose and practice)
+    it('Perform valid login', function(){
+        cy.visit('http://localhost:3000') 
+    })
+
+    it('Testfall 2', function(){
+         
+    })
+
+})
